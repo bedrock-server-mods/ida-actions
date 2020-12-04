e7dcd7b04d6f3559ba11d1a9058a6908437d368f
Frago9876543210 <al.volf.av@gmail.com>
2020-12-04 10:50:54 +0300

    Update analyze.ps1

--- analyze.ps1
+++ analyze.ps1
@@ -1,3 +1,4 @@
 cp database.idc 'C:\Program Files\IDA 7.2\idc\analysis.idc'
-& 'C:\Program Files\IDA 7.2\idat64.exe' -B bedrock_server -P+
+& 'C:\Program Files\IDA 7.2\idat64.exe' -B bedrock_server
+7z a bedrock_server.zip bedrock_server.i64
 #dir
