79eec3df5c5fada230b4a31ecb4d7999a113756c
Frago9876543210 <al.volf.av@gmail.com>
2021-07-13 23:33:04 +0300

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,6 +1,6 @@
 $ProgressPreference = "SilentlyContinue"
 
-$URL = "https://minecraft.net/en-us/download/server/bedrock/"
+$URL = "https://www.minecraft.net/en-us/download/server/bedrock/"
 $DOWNLOAD = (Invoke-WebRequest $URL).Content | Select-String -Pattern "https:\/\/\S*\/bin-linux\/\S*" | Foreach-Object {$_.Matches.Value}
 $DOWNLOAD = $DOWNLOAD.Substring(0, $DOWNLOAD.Length - 1)
 $VERSION = $DOWNLOAD | Select-String -Pattern "(\d+\.){3}\d+" | Foreach-Object {$_.Matches.Value}
