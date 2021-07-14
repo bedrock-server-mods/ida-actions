46a57f0257ad652876f78ee3e1401dfe63fbab3a
Frago9876543210 <al.volf.av@gmail.com>
2021-07-14 11:33:30 +0300

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,7 +1,7 @@
 $ProgressPreference = "SilentlyContinue"
 
 $URL = "https://www.minecraft.net/en-us/download/server/bedrock/"
-$DOWNLOAD = (Invoke-WebRequest $URL).Content | Select-String -Pattern "https:\/\/\S*\/bin-linux\/\S*" | Foreach-Object {$_.Matches.Value}
+$DOWNLOAD = (curl --silent --location --compressed --user-agent "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0" $URL).Content | Select-String -Pattern "https:\/\/\S*\/bin-linux\/\S*" | Foreach-Object {$_.Matches.Value}
 $DOWNLOAD = $DOWNLOAD.Substring(0, $DOWNLOAD.Length - 1)
 $VERSION = $DOWNLOAD | Select-String -Pattern "(\d+\.){3}\d+" | Foreach-Object {$_.Matches.Value}
 $OUTPUT = "bedrock_server.zip"
