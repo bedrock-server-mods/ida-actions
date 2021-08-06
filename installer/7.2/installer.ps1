c62dc0be19dc1174fe158de6230095d8835b8630
Frago9876543210 <al.volf.av@gmail.com>
2021-08-06 21:51:04 +0300

    try all versions

---
+++ installer/7.2/installer.ps1
@@ -0,0 +1,11 @@
+$IDA = "IDA PRO 7.2"
+$EXE = "x64_idapronw_hexx64w_181105_de455c480e11ef1ec91473028f4dd175.exe"
+$KEY = "install_key.txt"
+
+7z e ida.zip "$IDA/$EXE" "$IDA/$KEY"
+
+$PASSWORD = type $KEY
+& .\$EXE /VERYSILENT /PASSWORD=$PASSWORD | Out-Null
+
+reg import patch.reg
+echo "C:\Program Files\IDA 7.2"
