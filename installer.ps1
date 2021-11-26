7ecfa4558c9228d48ac470001eeb91dbf3099744
Frago9876543210 <al.volf.av@gmail.com>
2021-11-26 16:56:26 +0300

    here we go again...

--- installer.ps1
+++ installer.ps1
@@ -1,5 +1,7 @@
-mv ida.zip installer/${{ matrix.ida_version }}
-cd installer/${{ matrix.ida_version }}
+$IDA_VERSION = $args[0]
+
+mv ida.zip installer/$IDA_VERSION
+cd installer/$IDA_VERSION
 $IDA_DIR = .\installer.ps1 | Select-Object -Last 1
 cp ../../database.idc "$IDA_DIR\idc\analysis.idc"
 echo "IDA_DIR=$IDA_DIR" >> $env:GITHUB_ENV
